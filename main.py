6372e1dfad8716b8ed6656574902361060564336
zehralxn <zehra.korkusuz@hacettepe.edu.tr>
2021-07-04 19:30:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,15 +4,17 @@ from flask import Flask, render_template
 application = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@application.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
 
-@app.route('/subscribed', methods=['GET', 'POST'])
+@application.route('/subscribed', methods=['GET', 'POST'])
 def subscribed():
     return render_template('subscribed.html', scroll='subscribed')
 
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    application.debug = False
+    application.run()
+
